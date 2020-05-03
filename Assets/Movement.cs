bec500085d44318733e5c8a40cadf832918d455e
ekookk <echooliu@Gmail.com>
2020-05-02 19:14:09 -0700

    first commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 1.5f;
+    public Text countText;
+
+    private int count;
+
+    void Start()
+    {
+        count = 0;
+        SetCountText();
+
+    }
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("trash"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 20;
+            SetCountText();
+
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = count.ToString();
+    }
+}
